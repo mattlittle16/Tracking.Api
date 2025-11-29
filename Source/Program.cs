b87659d2147f05ea4a1cfc6b579b2be23a1ce21c
Matt Little <mituw16@gmail.com>
2025-11-29 16:22:17 -0500

    ups track response

--- Source/Program.cs
+++ Source/Program.cs
@@ -24,7 +24,11 @@ Log.Logger = new LoggerConfiguration()
 
 builder.Host.UseSerilog();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
